3b3a02ac8eb6b2807a0e1f555f2de957954eb591
fgarnadi <fajar.garnadi@gmail.com>
2021-12-07 00:27:41 +0700

    remove unused import

--- 2021/6.2.py
+++ 2021/6.2.py
@@ -1,25 +1,22 @@
-from sys import stdin
-from collections import Counter
-
 # represent fish life timer
 pool_size = 9
 
 days = 256
 
-if __name__ == '__main__':
-    initial = map(int, str(input()).split(','))
-    fishes = dict(Counter(initial))
+if __name__ == "__main__":
+    initial = map(int, str(input()).split(","))
+    fishes = [0] * pool_size
+    for fish in initial:
+        fishes[fish] += 1
 
     for _ in range(days):
         prev = 0
-        for i in range(pool_size-1, -1, -1):
-            fishes[i] = fishes.get(i, 0)
+        for i in range(pool_size - 1, -1, -1):
             if i == 0:
-                fishes[6] = fishes.get(6) + fishes.get(0)
-                fishes[8] = fishes.get(0)
+                fishes[6] = fishes[6] + fishes[0]
+                fishes[8] = fishes[0]
                 fishes[0] = prev
             else:
-                prev, fishes[i] = fishes.get(i), prev
+                prev, fishes[i] = fishes[i], prev
 
-    print(sum(x for x in fishes.values()))
-        
+    print(sum(fishes))
